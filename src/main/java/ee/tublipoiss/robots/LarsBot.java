08fc0f98b4a092920d97375c95a3e3fd9dcc9ef2
Lars Eckart <lars.eckart@gmail.com>
2017-11-11 18:05:07 +0200

    robots with kotlin!

--- src/main/java/ee/tublipoiss/robots/LarsBot.java
+++ src/main/java/ee/tublipoiss/robots/LarsBot.java
@@ -1,13 +1,13 @@
 package ee.tublipoiss.robots;
 
-//import java.awt.Color;
-
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
 import robocode.HitByBulletEvent;
 import robocode.HitWallEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
+
+import java.awt.Color;
 
 /**
  * LarsBot - a robot by Lars
@@ -17,16 +17,17 @@ public class LarsBot extends Robot {
     /**
      * run: LarsBot's default behavior
      */
+    @Override
     public void run() {
         // Initialization of the robot should be put here
 
         // After trying out your robot, try uncommenting the import at the top,
         // and the next line:
 
-        // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+        setColors(Color.black, Color.red, Color.yellow); // body,gun,radar
 
         // Robot main loop
-        while(true) {
+        while (true) {
             // Replace the next 4 lines with any behavior you would like
             ahead(100);
             turnGunRight(360);
@@ -38,6 +39,7 @@ public class LarsBot extends Robot {
     /**
      * onScannedRobot: What to do when you see another robot
      */
+    @Override
     public void onScannedRobot(ScannedRobotEvent e) {
         // Replace the next line with any behavior you would like
         fire(1);
@@ -46,6 +48,7 @@ public class LarsBot extends Robot {
     /**
      * onHitByBullet: What to do when you're hit by a bullet
      */
+    @Override
     public void onHitByBullet(HitByBulletEvent e) {
         // Replace the next line with any behavior you would like
         back(10);
@@ -54,6 +57,7 @@ public class LarsBot extends Robot {
     /**
      * onHitWall: What to do when you hit a wall
      */
+    @Override
     public void onHitWall(HitWallEvent e) {
         // Replace the next line with any behavior you would like
         back(20);
